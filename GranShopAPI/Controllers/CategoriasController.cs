7bf258bae8a7618755866df5df3079f7d7b520ac
luis-eduardo-hash <luis53duardo@gmail.com>
2025-05-29 19:08:57 -0300

    Atualizado1.2

--- GranShopAPI/Controllers/CategoriasController.cs
+++ GranShopAPI/Controllers/CategoriasController.cs
@@ -1,5 +1,6 @@
 
 using GranShopAPI.Data;
+using GranShopAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GranShopAPI.Controllers;
@@ -9,5 +10,26 @@ namespace GranShopAPI.Controllers;
 public class CategoriasController(AppDbContext db) : ControllerBase
 {
     private readonly AppDbContext _db = db;
-            
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_db.Categorias.ToList());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var categoria = _db.Categorias.Find(id);
+        if (categoria == null)
+            return NotFound();
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Categoria categoria)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Categoria")
+    }
 }
